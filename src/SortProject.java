69c5a6feb867d0fe1457f4b9fecc5ede48bcd8fd
mahdisaberi <mahdisaberi35@gmail.com>
2022-07-05 17:44:22 +0430

    using counting() for collection

--- src/SortProject.java
+++ src/SortProject.java
@@ -1,5 +1,8 @@
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.counting;
 
 public class SortProject {
     public static void main(String[] args) {
@@ -7,5 +10,6 @@ public class SortProject {
         ArrayList<String> names = new ArrayList(Arrays.asList("Amir", "Hatef", "Mehran", "Mojtaba", "Mohammad", "Ali", "Davood", "Reza","Mohsen"));
         System.out.println(names.stream().sorted((a, b) -> a.length() - b.length()).collect(Collectors.groupingBy(String::length)));
 
+        System.out.println(names.stream().sorted((a, b) -> a.length() - b.length()).collect(Collectors.groupingBy(String::length,counting())));
     }
 }
